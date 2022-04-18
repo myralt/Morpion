1eda6e4a089e7c60e0e0a8a8d7fcb232063738e8
myralt <no@email.com>
2022-04-18 16:17:12 +0200

    Node responds

---
+++ morpion-api.js
@@ -0,0 +1,15 @@
+const http = require("http");
+
+const toClient = (request, response) => {
+  const method = request.method;
+  const test = {
+    method: method,
+    message: "all good",
+  };
+  response.writeHead(200, { "Content-Type": "application/json" });
+  response.end(JSON.stringify(test));
+};
+
+const server = http.createServer(toClient);
+server.listen(8080);
+console.log("Server running at http://127.0.0.1:8080/");
