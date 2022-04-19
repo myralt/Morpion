18627839b41619ca6c19c44c40fbeaf6d3fab683
myralt <no@email.com>
2022-04-19 19:13:25 +0200

    Updated structure for file serve

--- morpion-api.js
+++ morpion-api.js
@@ -1,18 +1,18 @@
-const http = require("http");
+// const http = require("http");
 
-const toClient = (request, response) => {
-  const method = request.method;
-  const test = {
-    method: method,
-    message: "all good",
-  };
-  response.writeHead(200, {
-    "Content-Type": "application/json",
-    "Access-Control-Allow-Origin": "*",
-  });
-  response.end(JSON.stringify(test));
-};
+// const toClient = (request, response) => {
+//   const method = request.method;
+//   const test = {
+//     method: method,
+//     message: "all good",
+//   };
+//   response.writeHead(200, {
+//     "Content-Type": "application/json",
+//     "Access-Control-Allow-Origin": "*",
+//   });
+//   response.end(JSON.stringify(test));
+// };
 
-const server = http.createServer(toClient);
-server.listen(8080);
-console.log("Server running at http://127.0.0.1:8080/");
+// const server = http.createServer(toClient);
+// server.listen(8080);
+// console.log("Server running at http://127.0.0.1:8080/");
