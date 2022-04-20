3edafa806c66ee17fefe275304d4e2bc3ac41120
myralt <no@email.com>
2022-04-20 13:09:03 +0200

    Stores exact tile position from event, fixed bgd color bug

--- public/morpion.js
+++ public/morpion.js
@@ -1,5 +1,8 @@
-let playerPawns = [];
-let computerPawns = [];
+// CLIENT-SIDE LOGIC
+
+// All game variables ----------------------------------------------
+let playerTiles = [];
+let computerTiles = [];
 let winner;
 const gameBoard = document.getElementById("game-board");
 const maxGridSpace = 64;
@@ -7,25 +10,40 @@ const allCells = document.querySelectorAll("td > div");
 const playBtn = document.getElementById("play");
 const restartBtn = document.getElementById("restart");
 restartBtn.setAttribute("disabled", "true");
+
+// Page Actions -----------------------------------------------------
+
+// Enables interaction with board only when play button is clicked:
 playBtn.addEventListener("click", (event) => {
   playBtn.setAttribute("disabled", "true");
   restartBtn.removeAttribute("disabled");
   restartBtn.addEventListener("click", Restart);
   allCells.forEach((cell) => {
+    Reset(cell);
     cell.addEventListener("click", PlayerAction);
   });
 });
 
+// All functions ------------------------------------------------------
+
+// Sets tile at corresponding location and memorises it:
 function PlayerAction(event) {
   const target = event.explicitOriginalTarget;
-  target.innerHTML = "O";
-  playerPawns.push(target);
-  //const position = the grid coordinates as object
-  console.log("clicked");
+  target.textContent = "o";
+  target.setAttribute("class", "o");
+  playerTiles.push(target);
+  const x = target.parentNode;
+  const y = x.parentNode;
+  const position = {
+    x: x.cellIndex,
+    y: y.rowIndex,
+  };
+  console.log(position);
   VerifyBoard();
   //ComputerAction(position);
 }
 
+// Queries AI for next optimal move and sets tile accordingly:
 function ComputerAction(playerPosition) {
   fetch("http://127.0.0.1:8080")
     .then((response) => {
@@ -35,20 +53,21 @@ function ComputerAction(playerPosition) {
   //VerifyBoard();
 }
 
+// Removes all events and reinitialises game board and variables:
 function Restart() {
-  playerPawns = [];
-  computerPawns = [];
+  playerTiles = [];
+  computerTiles = [];
   allCells.forEach((cell) => {
     cell.removeEventListener("click", PlayerAction);
-    cell.innerHTML = "";
-    cell.style.backgroundColor = "grey";
+    Reset(cell);
   });
   restartBtn.setAttribute("disabled", "true");
   playBtn.removeAttribute("disabled");
 }
 
+// Checks if there are still empty slots and/or if there is a winner:
 function VerifyBoard() {
-  if (playerPawns.length + computerPawns.length === maxGridSpace || winner) {
+  if (playerTiles.length + computerTiles.length === maxGridSpace || winner) {
     allCells.forEach((cell) => {
       cell.removeEventListener("click", PlayerAction);
     });
@@ -59,4 +78,10 @@ function VerifyBoard() {
     console.log("Game over, no space left!");
     return 1;
   }
+}
+
+// Empties all slots on board:
+function Reset(slot) {
+  slot.removeAttribute("class");
+  slot.textContent = "";
 }
